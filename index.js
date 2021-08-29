55cb76a29c28ef2101a738c01288fa8c8a6e5ac2
Matt Miller <mmiller9913@gmail.com>
2021-08-29 16:30:08 -0400

    app

---
+++ index.js
@@ -0,0 +1,66 @@
+const CoinbasePro = require('coinbase-pro');
+require('dotenv').config({ path: 'variables.env' });
+
+const key = process.env.COINBASE_API_KEY;
+const secret = process.env.COINBASE_API_SECRET;
+const passphrase = process.env.COINBASE_API_PASSPHRASE;
+
+const apiURI = 'https://api.pro.coinbase.com';
+// const sandboxURI = 'https://api-public.sandbox.pro.coinbase.com';
+
+const authedClient = new CoinbasePro.AuthenticatedClient(
+    key,
+    secret,
+    passphrase,
+    apiURI
+);
+
+// const adaBuyParams = {
+//     'funds': 5, // USD,
+//     'currency': 'ADA',
+//     'type': 'market',
+//     'product_id': 'ADA-USD'
+// };
+
+const ethBuyParams = {
+    'funds': 5, // USD,
+    'currency': 'ETH',
+    'type': 'market',
+    'product_id': 'ETH-USD'
+};
+
+// const btcBuyParams = {
+//     'funds': 5, // USD,
+//     'currency': 'ETH',
+//     'type': 'market',
+//     'product_id': 'ETH-USD'
+// };
+
+
+const callback = (err, response, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+};
+
+
+setInterval(function () {
+    var date = new Date();
+    const hour = date.getHours();
+    // const minutes = date.getMinutes();
+    if (hour === 15) {
+        console.log('Bought ETH');
+        authedClient.buy(ethBuyParams, callback);
+    }
+}, 3600000) //run every hour
+
+//authedClient.buy(adaBuyParams, callback);
+// authedClient.buy(ethBuyParams, callback);
+// authedClient.buy(btcBuyParams, callback);
+
+// console.log(authedClient);
+
+//turorial
+//https://github.com/coinbase/coinbase-pro-node
